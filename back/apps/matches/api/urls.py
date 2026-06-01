5f772b422b8d4c354733f13ee5c9f747ecb0d366
EniKomani <ek70780@ubt-uni.net>
2026-06-01 16:09:47 +0200

    Added an endpoint match create

--- back/apps/matches/api/urls.py
+++ back/apps/matches/api/urls.py
@@ -1,17 +1,13 @@
 from django.urls import path
 from .views import (
     match_list_api,
-    match_detail_api,
-    match_create_api,
-    match_update_api,
-    match_delete_api,
-    match_add_player_api,
-    match_remove_player_api,
+   match_create_api,
 )
 
 app_name = "matches_api"
 
 urlpatterns = [
     path('', match_list_api, name='list'),
+    path('create/', match_create_api, name='create'),
 ]
 
