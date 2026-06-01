fd29284c491a2480c7b9f341316cccd00a27e795
EniKomani <ek70780@ubt-uni.net>
2026-06-01 16:30:15 +0200

    Added an endpoint matches update

--- back/apps/matches/api/urls.py
+++ back/apps/matches/api/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from .views import (
     match_list_api,
+    match_detail_api,
    match_create_api,
+    match_update_api,
 )
 
 app_name = "matches_api"
 
 urlpatterns = [
     path('', match_list_api, name='list'),
+    path('<int:pk>/', match_detail_api, name='detail'),
     path('create/', match_create_api, name='create'),
+    path('update/', match_update_api, name='update'),
 ]
 
