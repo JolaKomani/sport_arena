97e49c863734242b759a80b9ede715582bf2ec0f
FetaAvdiu <fettaavdiu@gmail.com>
2026-06-03 22:59:36 +0200

    Add user auth, performance and rating API endpoints

--- back/apps/users/api/urls.py
+++ back/apps/users/api/urls.py
@@ -18,5 +18,13 @@ app_name = "users_api"
 urlpatterns = [
     path('', user_list_api, name='list'),
     path('<int:pk>/', user_detail_api, name='detail'),
-
+    path('create/', user_create_api, name='create'),
+    path('update/', user_update_api, name='update'),
+    path('delete/', user_delete_api, name='delete'),
+    path('login/', user_login_api, name='login'),
+    path('logout/', user_logout_api, name='logout'),
+    path('me/', user_me_api, name='me'),
+    path('avg-rating/', user_avg_rating_api, name='avg-rating'),
+    path('performance/', user_performance_api, name='performance'),
+    path('avg-ratings/', players_avg_ratings_api, name='avg-ratings'),
 ]
