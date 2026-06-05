68e8193da8509dd1ba8fc127b7c3ebb035ddabba
laurentkadrijaj <laurentkadrijaj03@gmailcom>
2026-06-06 01:52:48 +0200

    Implement authentication module

---
+++ back/apps/core/auth/__init__.py
@@ -0,0 +1,17 @@
+from apps.core.auth.decorators import (
+    api_view,
+    jwt_optional,
+    jwt_required,
+    permission_required,
+    public_api,
+    role_required,
+)
+
+__all__ = [
+    'api_view',
+    'jwt_optional',
+    'jwt_required',
+    'permission_required',
+    'public_api',
+    'role_required',
+]
